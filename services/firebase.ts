d94a69a0f6bf696529fefd6a09ce850f25c2cd8e
SHISHANT BISWAS <135415806+shishantbiswas@users.noreply.github.com>
2024-03-14 16:32:17 +0530

    Update firebase.ts

--- services/firebase.ts
+++ services/firebase.ts
@@ -8,13 +8,14 @@ import { getStorage } from "firebase/storage";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId:process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENEDR_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
+  apiKey: 'AIzaSyAjxDcOZXEbfDw5l4W0Cnkk2bIrguDAj6g',
+  authDomain: "viteflix-v2.firebaseapp.com",
+  projectId: 'viteflix-v2',
+  storageBucket: 'viteflix-v2.appspot.com',
+  messagingSenderId:'179194005355',
+  appId: '1:179194005355:web:0c475da33123a239656e2e'
 };
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
